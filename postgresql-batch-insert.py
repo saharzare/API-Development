1bbf049a2eb7bd0f3e6f90a5dfbbe2fb27b90c05
Sahar Zare <79926856+saharzare@users.noreply.github.com>
2025-03-08 14:52:49 +0330

    Create postgresql-batch-insert.py

---
+++ postgresql-batch-insert.py
@@ -0,0 +1,46 @@
+import pandas as pd
+#extract data with api
+import requests
+#connect to database
+import psycopg2
+
+#extract data from api in a json format
+url='http://api.coincap.io/v2/assets'
+header={'Content-Type':'application/json','Accept-Encoding':'deflate'}
+response=requests.get(url,header)
+data=response.json()['data']
+df=pd.DataFrame(data)
+df.head(5)
+
+#connect to postgresql
+connects=psycopg2.connect(dbname='postgres',
+                          user='postgres',
+                          password='sahar',
+                          host='localhost',
+                          port='5432')
+
+cur=connects.cursor()
+cur.execute(""" 
+    CREATE TABLE IF NOT EXISTS CRYPTOTABLE (
+        id SERIAL PRIMARY KEY, 
+        rank INT, 
+        symbol VARCHAR, 
+        name VARCHAR, 
+        marketCapUsd FLOAT,
+        volumeUsd24Hr FLOAT, 
+        priceUsd FLOAT, 
+        changePercent24Hr FLOAT
+    );
+""")
+INSERT_QUERY=""" 
+INSERT INTO CRYPTOTABLE (rank,sym, name, marketCapUsd, volumeUsd24Hr, priceUsd, changePercent24Hr)
+VALUES (%s, %s, %s, %s, %s, %s, %s);
+"""
+#INSERT DATA INTO TABLES
+for index, row in df.iterrows():
+    cur.execute(INSERT_QUERY, (int(row['rank']), row['name'], row['symbol'], float(row['marketCapUsd']),float(row['volumeUsd24Hr']), float(row['priceUsd']), row['changePercent24Hr']))
+
+connects.commit()
+cur.close()
+connects.close()
+print("the insertation finished sucessefully")
